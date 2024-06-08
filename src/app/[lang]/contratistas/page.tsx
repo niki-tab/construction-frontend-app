28de669cbdff3ae3493e5bb47b28a9eaf9e7cac3
Nico <nicopolidis_12@hotmail.com>
2024-06-08 06:23:10 +0200

    zoho forms uploaded

--- src/app/[lang]/contratistas/page.tsx
+++ src/app/[lang]/contratistas/page.tsx
@@ -5,14 +5,14 @@ import Header from "@/components/header";
 export default function ContractorsPage({ params }: { params: { lang: string } }) {
   const lang = params.lang;
   return (
-    <div>
-      Contractor
+    <div className="h-dvh">
       <Header lang={lang}>
 
-    </Header>
-    <Footer lang={lang}>
+      </Header>
+      <iframe aria-label='Registro' frameborder="0" scrolling="no" style={{ height: '950px', width: '99%', border: 'none', overflow: 'hidden'}} src='https://forms.zohopublic.eu/nicolastabarestech/form/ContractorsLeadGenerationForm/formperma/sX62lkC9fxWYvXd8s55hJDKKMThgH4k0jIAxfugbXfQ'></iframe>
+      <Footer lang={lang}>
   
-  </Footer>
+      </Footer>
     </div>
   );
 }
--- src/app/[lang]/subcontratistas/page.tsx
+++ src/app/[lang]/subcontratistas/page.tsx
@@ -5,13 +5,13 @@ import Header from "@/components/header";
 export default function SubcontractorsPage({ params }: { params: { lang: string } }) {
   const lang = params.lang;
   return (
-    <div>
-      Subcontractor
+    <div className="h-dvh">
       <Header lang={lang}>
 
-    </Header>
-    <Footer lang={lang}>
-
+      </Header>
+      <iframe aria-label='Registro' frameborder="0" scrolling="no" style={{ height: '950px', width: '99%', border: 'none', overflow: 'hidden'}} src='https://forms.zohopublic.eu/nicolastabarestech/form/ContractorsLeadGenerationForm/formperma/sX62lkC9fxWYvXd8s55hJDKKMThgH4k0jIAxfugbXfQ'></iframe>
+      <Footer lang={lang}>
+  
       </Footer>
     </div>
   );
