0f7e4ee2046b6a9bfa97bd14c7f2b5aaf3a751c7
Nico <nicopolidis_12@hotmail.com>
2024-06-18 09:14:36 +0200

    form submissions

--- src/app/[lang]/subcontratistas/page.tsx
+++ src/app/[lang]/subcontratistas/page.tsx
@@ -3,34 +3,11 @@ import FooterDesktop from "@/components/footer/footer-desktop";
 import FooterMobile from "@/components/footer/footer-mobile";
 import HeaderDesktop from "@/components/header/header-desktop";
 import HeaderMobile from "@/components/header/header-mobile";
+import ContactForm1 from "@/components/shared/contact-form-1";
 
 export default function SubcontractorsPage({ params }: { params: { lang: string } }) {
   const lang = params.lang;
   return (
-    <form name="contact" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact" />
-      <p>
-<input type="text" name="firstname" id="firstname" />
-        <label htmlFor="yourname">
-          Your Name:
-        </label> <br />
-        <input type="text" name="name" id="yourname" />
-      </p>
-      <p>
-        <label htmlFor="youremail">
-          Your Email:
-        </label> <br />
-        <input type="email" name="email" id="youremail" />
-      </p>
-      <p>
-        <label htmlFor="yourmessage">
-          Message:
-        </label> <br />
-        <textarea name="message" id="yourmessage"></textarea>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+    <ContactForm1/>
   );
 }
