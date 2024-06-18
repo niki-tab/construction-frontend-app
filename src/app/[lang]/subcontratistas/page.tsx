955827f46921566870017295b6018e08a8cf7332
Nico <nicopolidis_12@hotmail.com>
2024-06-18 09:07:18 +0200

    form submissions

--- src/app/[lang]/subcontratistas/page.tsx
+++ src/app/[lang]/subcontratistas/page.tsx
@@ -7,38 +7,30 @@ import HeaderMobile from "@/components/header/header-mobile";
 export default function SubcontractorsPage({ params }: { params: { lang: string } }) {
   const lang = params.lang;
   return (
-    <div className="h-dvh">
-      <HeaderDesktop lang={lang}>
-      </HeaderDesktop>
-      <HeaderMobile lang={lang}>
-      </HeaderMobile>
-      <div className="h-full w-full bg-white pt-12 hidden md:block">
-      <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-      </div>
-    
-      <FooterDesktop lang={lang}>
-      </FooterDesktop>
-      <FooterMobile lang={lang}>
-      </FooterMobile>
-    </div>
+    <form name="contact" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+<input type="text" name="firstname" id="firstname" />
+        <label htmlFor="yourname">
+          Your Name:
+        </label> <br />
+        <input type="text" name="name" id="yourname" />
+      </p>
+      <p>
+        <label htmlFor="youremail">
+          Your Email:
+        </label> <br />
+        <input type="email" name="email" id="youremail" />
+      </p>
+      <p>
+        <label htmlFor="yourmessage">
+          Message:
+        </label> <br />
+        <textarea name="message" id="yourmessage"></textarea>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   );
 }
