9e76b50eea5dd44f0cb4390765625116b32151c1
Arb Rahim Badsa <arbrahimbadsa@gmail.com>
2024-06-18 15:11:35 +0600

    feat: add netlify form

--- src/components/shared/contact-form-1.tsx
+++ src/components/shared/contact-form-1.tsx
@@ -1,31 +1,60 @@
-"use client"
+"use client";
 
 export default function ContactForm1() {
-    return (
-      <form name="contact" method="POST" data-netlify="true">
-        <input type="hidden" name="form-name" value="contact" />
-        <p>
-  <input type="text" name="firstname" id="firstname" />
-          <label htmlFor="yourname">
-            Your Name:
-          </label> <br />
-          <input type="text" name="name" id="yourname" />
-        </p>
-        <p>
-          <label htmlFor="youremail">
-            Your Email:
-          </label> <br />
-          <input type="email" name="email" id="youremail" />
-        </p>
-        <p>
-          <label htmlFor="yourmessage">
-            Message:
-          </label> <br />
-          <textarea name="message" id="yourmessage"></textarea>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    )
-  }
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      const myForm = e.target;
+      const formData = new FormData(myForm);
+      const res = await fetch("/__forms.html", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData as any).toString(),
+      });
+      if (res.status === 200) {
+        alert("Message sent successfully");
+      } else {
+        alert(`${res.status} ${res.statusText}`);
+      }
+    } catch (e) {
+      alert(`${e}`);
+    }
+  };
+  return (
+    <form name="contact" onSubmit={handleSubmit}>
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label htmlFor="yourname">Your Name:</label>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter your name"
+          id="yourname"
+        />
+        <br />
+      </p>
+      <p>
+        <label htmlFor="youremail">Your Email:</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          id="youremail"
+        />
+        <br />
+      </p>
+      <p>
+        <label htmlFor="yourmessage">Message:</label>
+        <textarea
+          name="message"
+          placeholder="Enter your message"
+          id="yourmessage"
+        ></textarea>
+        <br />
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  );
+}
